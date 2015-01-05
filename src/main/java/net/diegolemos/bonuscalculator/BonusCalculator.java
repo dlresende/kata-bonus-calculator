21814202ee153173a681be232aff2dbff153de72
Diego Lemos <diegolemosderesende@gmail.com>
2015-01-05 15:40:24 +0100

    Clean net.diegolemos.bonuscalculator.BonusCalculator.

--- src/main/java/net/diegolemos/bonuscalculator/BonusCalculator.java
+++ src/main/java/net/diegolemos/bonuscalculator/BonusCalculator.java
@@ -2,37 +2,39 @@ package net.diegolemos.bonuscalculator;
 
 import static java.lang.String.format;
 
-import java.util.Set;
-
 public class BonusCalculator
 {
-    private final Set<Employee> employees;
-
-    public BonusCalculator(Set<Employee> employees)
-    {
-        this.employees = employees;
+    public double calculateLeftover(double turnover, Employee... employees) {
+        print("Turnover: $ %.2f.", turnover);
+        double bonuses = sumBonuses(turnover, employees);
+        double leftover = turnover - bonuses;
+        print("Leftover: $ %.2f.", leftover);
+		return leftover;   
     }
 
-    public double payEveryone(double turnover)
-    {
-        print("Turnover: $ %.2f.", turnover);
-        double leftover = turnover;
+
+    private double sumBonuses(double turnover, Employee... employees) {
+		double bonuses = 0.0;
 
         for(Employee employee : employees)
         {
-            double employeeBonus = employee.calculateBonus(turnover);
-            leftover -= employeeBonus;
-
-            if(employeeBonus > 0.0)
-            	print("Yeah! %s got payed $ %.2f.", employee, employeeBonus);
-            else
-                print("Oops! %s does not have bonus.", employee);
+            bonuses += calculateBonus(turnover, employee);
         }
+        
+		return bonuses;
+	}
 
-        print("Leftover: $ %.2f.", leftover);
 
-        return leftover;
-    }
+	private double calculateBonus(double turnover, Employee employee) {
+		double employeeBonus = employee.calculateBonus(turnover);
+
+		if(employeeBonus > 0.0)
+			print("Yeah! %s got payed $ %.2f.", employee, employeeBonus);
+		else
+		    print("Oops! %s does not have bonus.", employee);
+		
+		return employeeBonus;
+	}
 
 	private void print(String template, Object... values) {
 		System.out.println(format(template, values));
--- src/test/java/net/diegolemos/bonuscalculator/BonusCalculatorTest.java
+++ src/test/java/net/diegolemos/bonuscalculator/BonusCalculatorTest.java
@@ -1,27 +1,24 @@
 package net.diegolemos.bonuscalculator;
 
-import org.junit.Test;
-
-import java.util.HashSet;
-import java.util.Set;
-
 import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
 
 public class BonusCalculatorTest
 {
-    @Test
-    public void should_pay_everyone()
-    {
-        Set<Employee> employees = new HashSet<>();
-        employees.add(new Employee("John"));
-        employees.add(new Manager("Sara", 10));
-        employees.add(new ProgrammerJunior("Joe", true));
-        employees.add(new ProgrammerJunior("Billy", false));
-        employees.add(new ProgrammerSenior("Elisabeth", true));
-        employees.add(new Secretary("Marie", 130));
-        BonusCalculator accountant = new BonusCalculator(employees);
+    private static final Secretary MARIE = new Secretary("Marie", 130);
+	private static final ProgrammerSenior ELIZABETH = new ProgrammerSenior("Elisabeth", true);
+	private static final ProgrammerJunior BILLY = new ProgrammerJunior("Billy", false);
+	private static final ProgrammerJunior JOE = new ProgrammerJunior("Joe", true);
+	private static final Manager SARA = new Manager("Sara", 10);
+	private static final Employee JOHN = new Employee("John");
 
-        double leftover = accountant.payEveryone(100_000.00);
+	@Test
+    public void should_pay_all_employees_and_calculate_the_leftover()
+    {
+        BonusCalculator calculator = new BonusCalculator();
+
+        double leftover = calculator.calculateLeftover(100_000.00, JOHN, SARA, JOE, BILLY, ELIZABETH, MARIE);
 
         assertThat(leftover).isEqualTo(100_000.00 * (1 - ((8.5 + 2 * 2.27 + 5.32 + 1) / 100)));
     }
